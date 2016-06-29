7b372c188a273db983e2b077c8cb31dcad276763
charlesvardeman <charles.vardeman@gmail.com>
2016-06-29 10:16:45 -0400

    Adding python db converter.

---
+++ mat_triplify.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''dfs
+
+Usage:
+  dfs  FILE
+  dfs -h | --help
+  dfs --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+'''
+
+from __future__ import unicode_literals, print_function
+
+__version__ = "0.1.0"
+__author__ = "Charles Vardeman"
+__license__ = "MIT"
