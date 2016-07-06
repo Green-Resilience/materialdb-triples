56827cccc9df0f3fc2adc18d3fc2d2f9e0c624b1
charlesvardeman <charles.vardeman@gmail.com>
2016-07-06 09:38:45 -0400

    Prints csv triples up to isWindow.

--- mat_triplify.py
+++ mat_triplify.py
@@ -15,6 +15,9 @@ Options:
 from __future__ import unicode_literals, print_function
 from docopt import docopt
 import csv
+import rdflib
+from rdflib import Literal, BNode, Namespace, URIRef, Graph, Dataset, RDF, RDFS, XSD
+import uuid
 
 __version__ = "0.1.0"
 __author__ = "Charles Vardeman"
@@ -23,14 +26,35 @@ __license__ = "MIT"
 
 
 
+
+
 def main():
     """Main entry point for the dfs CLI."""
     args = docopt(__doc__, version=__version__)
     csvfile = args["FILE"]
+
+    PROV = Namespace("http://www.w3.org/ns/prov#")
+    QUDT = Namespace("http://qudt.org/schema/qudt#")
+    COMPONENT = Namespace("http://crc.nd.edu/schema/component#")
+
+
+    ds = rdflib.Dataset(default_union=True)
+    ds.bind("prov", PROV)
+    ds.bind("qudt", QUDT)
+    ds.bind("component", COMPONENT)
+
+
     with open(csvfile, 'rb') as f:
         reader = csv.reader(f)
         for row in reader:
-            print(row[1])
+          # generate new uuid for component
+            componentid = 'urn:green-matdb:' + str(uuid.uuid4())
+            ds.add((URIRef(componentid), RDF.type, COMPONENT.Component))
+            ds.add((URIRef(componentid), COMPONENT.gbxmlname, Literal(row[1])))
+            ds.add((URIRef(componentid), COMPONENT.archname, Literal(row[2])))
+            if (row[3] == '1'):
+                ds.add((URIRef(componentid), RDF.type, COMPONENT.Window))
+    print(ds.serialize(format="turtle"))
 
 
 if __name__ == '__main__':
