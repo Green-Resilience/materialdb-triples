b6e5b741312bd8614e15a843bf4d2e7a843d8100
charlesvardeman <charles.vardeman@gmail.com>
2016-07-06 08:23:08 -0400

    Read in gbxmlname.

--- mat_triplify.py
+++ mat_triplify.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''dfs
+"""dfs
 
 Usage:
   dfs  FILE
@@ -10,10 +10,28 @@ Usage:
 Options:
   -h --help     Show this screen.
   --version     Show version.
-'''
+"""
 
 from __future__ import unicode_literals, print_function
+from docopt import docopt
+import csv
 
 __version__ = "0.1.0"
 __author__ = "Charles Vardeman"
 __license__ = "MIT"
+
+
+
+
+def main():
+    """Main entry point for the dfs CLI."""
+    args = docopt(__doc__, version=__version__)
+    csvfile = args["FILE"]
+    with open(csvfile, 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(row[1])
+
+
+if __name__ == '__main__':
+    main()
